2cb031040e40fd800c9c17ab229b1ab8e699a3eb
willy903 <willyjacquino@gmail.com>
2022-09-13 18:31:20 +0100

    Premier upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { GameComponent } from './components/game/game.component';
+import { EshopComponent } from './components/eshop/eshop.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
+
+const routes: Routes = [
+  { path: '',redirectTo:"/home",pathMatch:"full"},
+  { path: 'home', component: HomeComponent},
+  { path: 'game', component: GameComponent},
+  { path: 'eshop', component: EshopComponent},
+  { path: 'moncompte', component: MonCompteComponent },
+  { path: '**', component: PagenotfoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { GameComponent } from './components/game/game.component';
+import { EshopComponent } from './components/eshop/eshop.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
+import { BarNavComponent } from './components/bar-nav/bar-nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { AsideRightComponent } from './components/aside-right/aside-right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameComponent,
+    EshopComponent,
+    MonCompteComponent,
+    BarNavComponent,
+    FooterComponent,
+    PagenotfoundComponent,
+    AsideRightComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
